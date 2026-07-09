301781942be1b185cf1e1094b97c9bd9c28f7d1f
Nishant Jha <nishantjha399@gmail.com>
2026-07-09 10:30:44 +0530

    project

--- Frontend/react_basic/counterapp/counter/src/App.jsx
+++ Frontend/react_basic/counterapp/counter/src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {increment , decrement} from './store/counterSlice'
 
 const App = () => {
+
+  let count = useSelector((state)=>state.counter.value)
+  let dispatch = useDispatch()
+  console.log(count);
+  
   return (
     <div className='container'>
-      <h1>counterapp</h1>
-      <div></div>
-      
+      <h1>Counter app</h1>
+      <br />
+      <br />
+      <div className='count' >
+        <button
+        onClick={()=>{
+          dispatch(increment())
+        }}
+        >incre....</button>
+        <h1>{count}</h1>
+        <button
+        onClick={()=>{
+          dispatch(decrement())
+        }}
+        >Decre....</button>
+      </div>
     </div>
   )
 }
